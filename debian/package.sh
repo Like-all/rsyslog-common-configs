5d3dd37445444070606fb82189633196771ab447
Azer Abdullaev <buckstabu@yandex-team.ru>
2015-04-24 11:12:18 +0300

    Initial release: splitting mpdscribble to separate file

---
+++ debian/package.sh
@@ -0,0 +1,7 @@
+export DEBFULLNAME="Azer Abdullaev (Like All)"
+export DEBEMAIL="like-all@yandex.com"
+
+NAME="rsyslog-common-configs"
+VERSION="0.1"
+SUFFIX="2"
+FLAVOURS="wheezy,jessie,precise,trusty"
